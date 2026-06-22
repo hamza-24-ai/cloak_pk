ff2303fafc00c3811ee145037e7acc83fb00913f
Muhammad Hamza <mhuzsifa684@gmail.com>
2026-06-22 19:12:59 +0500

    FiX CORS Policy

--- backened/app/main.py
+++ backened/app/main.py
@@ -27,7 +27,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["https://cloak-pk.vercel.app/"],
+    allow_origins = ["*"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
